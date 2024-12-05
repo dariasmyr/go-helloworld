8539ba42e2ad42271b9a903c748626791ec5c360
smyr <dasha.smyr@gmail.com>
2024-12-05 15:49:38 +0300

    Added worked pool and job structs

---
+++ worker_pool/job.go
@@ -0,0 +1,42 @@
+package worker_pool
+
+import "context"
+
+type Job struct {
+	Description JobDescriptor
+	ExecFn      ExecutionFn
+	Args        interface{}
+}
+
+type ExecutionFn func(ctx context.Context, args interface{}) (interface{}, error)
+
+type JobID string
+type jobType string
+type jobMetadata map[string]interface{}
+
+type JobDescriptor struct {
+	ID       JobID
+	JobType  jobType
+	Metadata jobMetadata
+}
+
+type Result struct {
+	Value       interface{}
+	Err         error
+	Description JobDescriptor
+}
+
+func (j Job) execute(ctx context.Context) Result {
+	value, err := j.ExecFn(ctx, j.Args)
+	if err != nil {
+		return Result{
+			Err:         err,
+			Description: j.Description,
+		}
+	}
+
+	return Result{
+		Value:       value,
+		Description: j.Description,
+	}
+}
---
+++ worker_pool/worker.go
@@ -0,0 +1,56 @@
+package worker_pool
+
+import (
+	"context"
+	"fmt"
+	"sync"
+)
+
+type WorkerPool struct {
+	workersCount int
+	jobs         chan Job
+	results      chan Result
+	Done         chan struct{}
+}
+
+func (wp WorkerPool) GenerateFrom(jobsBulk []Job) {
+	for i := range jobsBulk {
+		wp.jobs <- jobsBulk[i]
+	}
+
+	close(wp.jobs)
+}
+
+func (wp WorkerPool) Run(ctx context.Context) {
+	var wg sync.WaitGroup
+
+	for i := 0; i < wp.workersCount; i++ {
+		wg.Add(1)
+		go worker(ctx, &wg, wp.jobs, wp.results)
+	}
+
+	wg.Wait()
+	close(wp.Done)
+	close(wp.results)
+}
+
+func worker(ctx context.Context, wg *sync.WaitGroup, jobs <-chan Job, results chan<- Result) {
+	defer wg.Done()
+
+	for {
+		select {
+		case job, ok := <-jobs:
+			if !ok {
+				return
+			}
+			results <- job.execute(ctx)
+		case <-ctx.Done():
+			fmt.Printf("cancelled worker. Error detail: %v/n", ctx.Err())
+			results <- Result{
+				Err: ctx.Err(),
+			}
+			return
+		}
+
+	}
+}
