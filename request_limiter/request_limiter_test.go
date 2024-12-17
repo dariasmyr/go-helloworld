af854e5ed2cef84ec9d55241233de9944d2643fd
smyr <dasha.smyr@gmail.com>
2024-12-17 16:13:55 +0300

    Added limiter per request

--- request_limiter/request_limiter_test.go
+++ request_limiter/request_limiter_test.go
@@ -55,6 +55,32 @@ func (s *Service) HandleAllRequests(process func(), u *User) bool {
 	}
 }
 
+func (s *Service) HandleRequest(process func(), u *User) bool {
+	done := make(chan bool)
+
+	go func() {
+		process()
+		done <- true
+	}()
+
+	success := true
+
+	fmt.Printf("Started handling request for user: %s\n", u.ID)
+
+	select {
+	case <-done:
+		fmt.Printf("Process func has been done for user: %s\n", u.ID)
+		return success
+	case <-time.After(time.Second * time.Duration(s.LimitSec)):
+		if !u.IsPremium {
+			fmt.Printf("Time used for processing has reached the limit of %v sec \n", s.LimitSec)
+			return false
+		}
+	}
+
+	return success
+}
+
 func sampleProcess(seconds int) {
 	start := time.Now()
 
@@ -77,6 +103,27 @@ func TestHandlingAllRequests(t *testing.T) {
 		for i := 1; i <= 15; i++ {
 			successful := service.HandleAllRequests(func() { sampleProcess(2) }, user)
 			log.Printf("Finished short process %v with success: %v, user premium: %v \n", i, successful, user.IsPremium)
+		}
+	})
+}
+
+func TestHandlingRequest(t *testing.T) {
+	t.Run(t.Name(), func(t *testing.T) {
+		service := &Service{
+			LimitSec: 3,
+		}
+
+		user := &User{
+			ID:        uuid.NewString(),
+			IsPremium: false,
+		}
+
+		for i := 1; i <= 5; i++ {
+			successful := service.HandleRequest(func() { sampleProcess(2) }, user)
+			log.Printf("Finished short process %v with success: %v, user premium: %v \n", i, successful, user.IsPremium)
+
+			nonsuccessfull := service.HandleRequest(func() { sampleProcess(4) }, user)
+			log.Printf("Finished short process %v with success: %v, user premium: %v \n", i, nonsuccessfull, user.IsPremium)
 		}
 	})
 }
