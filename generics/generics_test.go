f8a6fe851f8821e49f108cbb0ccfe5a6fedcc28e
daria_smyr <dasha.smyr@gmail.com>
2025-02-02 16:34:26 +0300

    Added test for generics usage only

--- generics/generics_test.go
+++ generics/generics_test.go
@@ -42,18 +42,26 @@ func getDataWithReflectAndGenerics[T any](data interface{}) []T {
 	return nil
 }
 
+// Works only for struct with one field Data of type []T
+func getDataWithGenericsOnly[T any](data struct{ Data []T }) []T {
+	return data.Data
+}
+
 func TestReflect(t *testing.T) {
 
 	data1 := data1{Data: []int{1, 2, 3}}
 	fmt.Println("data1:", getDataWithReflectOnly(data1))
 	fmt.Println("data1:", getDataWithReflectAndGenerics[int](data1))
+	fmt.Println("data1:", getDataWithGenericsOnly(data1))
 
 	data2 := data2{Data: []string{"A", "B", "C"}}
 	fmt.Println("data2:", getDataWithReflectOnly(data2))
 	fmt.Println("data2:", getDataWithReflectAndGenerics[string](data2))
+	fmt.Println("data2:", getDataWithGenericsOnly(data2))
 
 	data3 := data3{Data: []byte{1, 2, 3}}
 	fmt.Println("data3:", getDataWithReflectOnly(data3))
 	fmt.Println("data3:", getDataWithReflectAndGenerics[byte](data3))
+	fmt.Println("data3:", getDataWithGenericsOnly(data3))
 
 }
