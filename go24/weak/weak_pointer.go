58132bc185086cdd0e9e78db3fbda5d66cfa3459
smyr <dasha.smyr@gmail.com>
2025-02-13 14:46:45 +0300

    Added weak vs strong pointer comparing

--- go24/weak/weak_pointer.go
+++ go24/weak/weak_pointer.go
@@ -31,6 +31,12 @@ func testGC() {
 
 	fmt.Printf("Before GC: weakPointer.Value() = %+v\n", weakPointer.Value())
 
+	if st := weakPointer.Value(); st != x {
+		fmt.Printf("weak pointer is not the same as strong pointer: %p vs. %p\n", st, x)
+	} else {
+		fmt.Printf("weak pointer value is the same as strong pointer: %p vs. %p\n", st, x)
+	}
+
 	//Call GC manually, x poiner is released as there is ni usage of it in the code
 	runtime.GC()
 
