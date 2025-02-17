a3ef695b4a64a9aee910ecf199468c8c8274a02f
smyr <dasha.smyr@gmail.com>
2025-02-17 17:24:13 +0300

    ./

--- go24/generic_type_aliases/generic_type_aliases.go
+++ go24/generic_type_aliases/generic_type_aliases.go
@@ -2,15 +2,21 @@ package main
 
 import "fmt"
 
-type IntAlias = int
 type StringAlias = string
-type SetAlias[P comparable] = map[P]bool
 
+type SetAlias[P comparable] = map[P]bool
 type SetString = SetAlias[string]
 type SetInt = SetAlias[int]
 
+type TypeValue interface {
+	int | string
+}
+
+type A[P comparable] = TypeValue
+type B = A[bool]
+
 type AllowedTypes interface {
-	IntAlias | StringAlias | SetInt | SetString
+	StringAlias | SetInt | SetString | B
 }
 
 func NewBox[T AllowedTypes](value T) {
@@ -18,9 +24,11 @@ func NewBox[T AllowedTypes](value T) {
 }
 
 func main() {
-	NewBox(42)
 	NewBox("Hello Go")
 
 	NewBox(map[string]bool{"a": true, "b": false})
-	// NewBox(3.14)
+
+	NewBox(3)
+
+	//NewBox(true) не пройдет, так как не bool does not satisfy AllowedTypes
 }
