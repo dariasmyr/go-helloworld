185601404b25e1382b9992faea60757b27fbb5cd
smyr <dasha.smyr@gmail.com>
2025-06-12 14:56:54 +0300

    Fixed simple worker pool

--- worker_pool/simplest_worker_pool_test.go
+++ worker_pool/simplest_worker_pool_test.go
@@ -1,47 +1,130 @@
 package worker_pool
 
 import (
+	"context"
 	"fmt"
 	"sync"
 	"testing"
+	"time"
 )
 
-func simpleWorker(tasks <-chan string, results chan<- map[string]int, wg *sync.WaitGroup) {
-	defer wg.Done()
-	localResult := make(map[string]int)
-	for task := range tasks {
-		processed := len(task)
-		localResult[task] = processed
+const (
+	defaultMaxWorkers = 10
+	defaultDur        = 250 * time.Millisecond
+)
+
+type Task struct{ ID int }
+type Res struct {
+	taskId int
+	status bool
+}
+
+type SimpleWorkerPool struct {
+	tasks      chan Task
+	res        chan Res
+	dur        time.Duration
+	ctx        context.Context
+	cancel     context.CancelFunc
+	maxWorkers int
+}
+
+func simpleWorker(ctx context.Context, tasks <-chan Task, results chan<- Res) {
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		case task, ok := <-tasks:
+			if !ok {
+				fmt.Printf("Chan tasks closed\n")
+				return
+			}
+			fmt.Printf("Processing task %d\n", task.ID)
+			results <- Res{
+				taskId: task.ID,
+				status: true,
+			}
+		}
 	}
-	results <- localResult
+}
+
+func NewWorkerPool(maxWorkers int, dur time.Duration, maxQueueSize int, ctx context.Context, cancel context.CancelFunc) *SimpleWorkerPool {
+	if dur == 0 {
+		dur = defaultDur
+	}
+
+	if maxWorkers == 0 {
+		maxWorkers = defaultMaxWorkers
+	}
+
+	return &SimpleWorkerPool{
+		tasks:      make(chan Task, maxQueueSize),
+		res:        make(chan Res),
+		dur:        dur,
+		maxWorkers: maxWorkers,
+		ctx:        ctx,
+		cancel:     cancel,
+	}
+}
+
+func (wp *SimpleWorkerPool) Run(wg *sync.WaitGroup) {
+	for i := 0; i < wp.maxWorkers; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			simpleWorker(wp.ctx, wp.tasks, wp.res)
+		}()
+	}
 }
 
 func TestWorkerPool(t *testing.T) {
-	tasks := make(chan string, 10)
-	results := make(chan map[string]int, 5)
 	var wg sync.WaitGroup
+	ctx, cancel := context.WithCancel(context.Background())
 
-	for i := 0; i < 5; i++ {
-		wg.Add(1)
-		go simpleWorker(tasks, results, &wg)
-	}
+	wp := NewWorkerPool(0, 0, 10, ctx, cancel)
 
+	wp.Run(&wg)
+
+	go wp.readRes()
 	go func() {
-		for _, task := range []string{"task1", "task2", "task3"} {
-			tasks <- task
-		}
-		close(tasks)
+		wg.Wait()
+		close(wp.res)
 	}()
 
-	wg.Wait()
-	close(results)
+	for i := 0; i <= 50; i++ {
+		AddTask(ctx, i, wp.tasks, wp.dur)
+	}
+	close(wp.tasks)
+}
 
-	finalResult := make(map[string]int)
-	for res := range results {
-		for k, v := range res {
-			finalResult[k] = v
+func (wp *SimpleWorkerPool) Stop() {
+	wp.cancel()
+}
+
+func (wp *SimpleWorkerPool) readRes() {
+	for {
+		select {
+		case <-wp.ctx.Done():
+			fmt.Printf("Context closed\n")
+			return
+		case res, ok := <-wp.res:
+			if !ok {
+				fmt.Printf("Res chan closed\n")
+				return
+			}
+			fmt.Printf("Res %d; status %v\n", res.taskId, res.status)
 		}
 	}
+}
 
-	fmt.Println(finalResult)
+func AddTask(ctx context.Context, n int, tasks chan<- Task, dur time.Duration) {
+	select {
+	case <-ctx.Done():
+		fmt.Printf("Context closed\n")
+		return
+	case tasks <- Task{ID: n}:
+		fmt.Printf("Task %d sent to tasks chan\n", n)
+	case <-time.After(dur):
+		fmt.Printf("Timer ticked, skip task sending\n")
+		return
+	}
 }
