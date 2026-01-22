3cc5a4f02ae4fd7bc3615f65772172090b8545ee
darya.smyr <darya.smyr@lamoda.ru>
2026-01-22 14:21:53 +0300

    Add bloom filter example realisation

---
+++ bloom/bloom_filter_test.go
@@ -0,0 +1,18 @@
+package bloom
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestBloomFilter(t *testing.T) {
+	bf := NewBloomFilter(1000, 10, 3)
+
+	item1 := []byte("apple")
+	item2 := []byte("banana")
+
+	bf.Add(item1)
+
+	fmt.Println("Contains 'apple':", bf.Contains(item1))
+	fmt.Println("Contains 'banana':", bf.Contains(item2))
+}
